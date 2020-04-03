81ba787316e1763d8af49223f8d7ae448480a2ff
chaithanyasindhusree <chaithanyasindhusree@gmail.com>
2020-04-03 08:31:04 +0530

    Adding files to Git Repo

---
+++ A1.java
@@ -0,0 +1,25 @@
+class A1
+{
+
+  int i;
+  
+  int j;
+  
+  A1()
+  {
+	  for(i=0;i<20;i++)
+		  j=i;
+	  System.out.println(j);
+  }
+  
+public static void main(String args[])
+
+{
+  A1 a=new A1();
+ System.out.println(a.i); 
+System.out.println("Hello, World!"); 
+
+
+}
+
+}
